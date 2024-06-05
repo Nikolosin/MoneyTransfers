bfbeec8765574bc9ae0c85e9d24d7bfab822721f
Nikolay Petrenko <nikolosin73@gmail.com>
2024-06-05 14:06:14 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package ru.netology
+
+import kotlin.math.max
+
+fun main() {
+    val amount = 456578
+    val minCommission = 35.0
+    val commission = (max(minCommission, amount*0.0075)).toInt()
+    println("Комиссия составит $commission руб.")
+}
